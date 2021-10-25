2002a9006b2c71ff22251ccdbd52ce2eb4f48a0a
TeKa <kiener.thomas@gmail.com>
2021-10-25 16:20:43 +0200

    TK-1025-mvp-back-run.py

--- product/back/run.py
+++ product/back/run.py
@@ -2,6 +2,6 @@
 from app.views import app
 
 if __name__ == "__main__":
-    # socketio.run(app, debug=True)
-    app.run(host='0.0.0.0', debug=True)
+    socketio.run(app, debug=True)
+    # app.run(host='0.0.0.0', debug=True)
     print("run run.py")
