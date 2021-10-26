db161449434c3639fb4159b8307a13ee50a04f12
Pierre <Pierre>
2021-10-26 15:22:17 +0200

    liste de conversations en MUI et ajout de create-conversation avec modif dans le back

--- product/front/src/ConversationsByRows.jsx
+++ product/front/src/ConversationsByRows.jsx
@@ -1,10 +1,29 @@
 import * as React from 'react';
 import './ConversationsByRows.css'
+import Box from '@mui/material/Box';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import Divider from '@mui/material/Divider';
+import Typography from '@mui/material/Typography';
 
 export default function ConversationsByRows(props) {
   return (
-    <a href={`/conversation/${props.id}`}>
-      <div className="conversations-rows">{props.name}</div>
-    </a>
+    <Box sx={{ width: '100%', bgcolor: 'background.paper' }}>
+      <Divider />
+      <nav>
+        <List>
+        <a className="list-conv" href={`/conversation/${props.id}`}>
+          <ListItem disablePadding>
+            <ListItemButton>
+            <Typography variant="h5" gutterBottom component="div">
+              {props.name}
+            </Typography>
+            </ListItemButton>
+          </ListItem>
+          </a>
+        </List>
+      </nav>
+    </Box>
   );
 }
