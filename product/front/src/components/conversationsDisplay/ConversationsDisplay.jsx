e6d40bfe796de27ecf2326b6fa604c5ffd1ab9d8
TeKa <kiener.thomas@gmail.com>
2021-11-15 13:48:49 +0100

    TK-1115-merge-Aude811Soir->main

--- product/front/src/components/conversationsDisplay/ConversationsDisplay.jsx
+++ product/front/src/components/conversationsDisplay/ConversationsDisplay.jsx
@@ -11,7 +11,7 @@ export default function ConversationsDisplay(props) {
   return (
     <Box sx={{ width: "100%", bgcolor: "background.paper" }}>
       <Divider />
-      <nav>
+      <nav className = "bgColor">
         <List>
           <a className='list-conv' href={`/${props.user_id}/conversation/${props.id}`}>
             <ListItem disablePadding>
